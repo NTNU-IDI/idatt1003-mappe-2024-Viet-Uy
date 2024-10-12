1835ee2afbb2201aff35533867767ad62c4f6d17
Viet-Uy <vietuydo@gmail.com>
2024-10-12 23:20:07 +0200

    test from home

--- Foodsystem/src/main/java/edu/ntnu/idi/bidata/Main.java
+++ Foodsystem/src/main/java/edu/ntnu/idi/bidata/Main.java
@@ -2,6 +2,10 @@ package edu.ntnu.idi.bidata;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("test push");
+        System.out.println("test push");
+        System.out.println("test push");
+
     }
 }
---
+++ Foodsystem/src/test/java/edu/ntnu/idi/bidata/MainTest.java
@@ -0,0 +1,6 @@
+package edu.ntnu.idi.bidata;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MainTest {
+  
+}
